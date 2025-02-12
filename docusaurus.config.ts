fb515cb5245c2ac9812fb2f7647bf6afdf237161
Bjoern <bjoern.wagner@gmail.com>
2025-02-12 11:45:18 -0700

    added og image to repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,7 +91,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/dialect-social-card.png",
     navbar: {
       title: "Dialect",
       logo: {
