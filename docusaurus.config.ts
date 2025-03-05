ce451bc1eb3a9d1df161f8ebee5064354f2220a9
Bjoern <bjoern.wagner@gmail.com>
2025-03-05 19:19:56 +0100

    feat: added search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -207,6 +207,38 @@ const config: Config = {
       ],
       copyright: `©${new Date().getFullYear()} Dialect Labs - All rights reserved`,
     },
+    algolia: {
+      // The application ID provided by Algolia
+      appId: "7YE7CPM0GQ",
+
+      // Public API key: it is safe to commit it
+      apiKey: "7f55e1f02365e68027c13871e3c7e5b8",
+
+      indexName: "dialect",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: "external\\.com|domain\\.com",
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: "/docs/", // or as RegExp: /\/docs\//
+        to: "/",
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
